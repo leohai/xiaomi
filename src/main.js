91f7dfaaa180d3ef506d8e33276540e5eafa7553
leohai <hail@haideMacBook-Air.local>
2020-02-13 18:06:16 +0800

    feat:axios全局拦截和mock数据

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
+// import env from './env'
+
+let mock = true
+if (mock) {
+  require('./mock/api')
+}
+axios.defaults.baseURL = '/api'
+// axios.defaults.baseURL = env.baseURL
+axios.defaults.timeout = 8000;
+axios.interceptors.response.use((response) => {
+  let res = response.data
+  if (res.status == 0) {
+    return res.data
+  } else if (res.status == 10) {
+    window.location.href = "/#/login"
+  } else {
+    alert(res.msg)
+  }
+}, (error) => {
+  // let res = error.response;
+  return Promise.reject(error);
+})
+
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 new Vue({
   router,
--- src/mock/api.js
+++ src/mock/api.js
@@ -1,10 +1,10 @@
 import Mock from 'mockjs'
-Mock.mock('/api/user/login',{
+Mock.mock('/api/user/login', {
   "status": 0,
   "data": {
     "id|10001-11000": 0,
     "username": "@cname",
-    "email": "admin@51purse.com",
+    "email": "@email",
     "phone": null,
     "role": 0,
     "createTime": 1479048325000,
